66956e88b5f81071534921d64315733d07acc12b
@marespinosa <maricon@legendhasit.co.nz>
2024-08-15 13:25:18 +0800

    update files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './App.css'
+import Post from './components/Post'
+
+function App() {
+  return  <Post />;
+}
+
+export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+const names = ['Manuel','Marie'];
+
+
+function Post(){
+ return (
+    <div>
+        <p>Hi Mari!</p>
+        <p>React Js is awesome!</p>
+    </div>
+ );
+}
+
+export default Post;
